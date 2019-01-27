2902cef30fb758a126275a0b8d696d791f484877
Cerem Cem ASLAN <cem@aktos.io>
2019-01-28 00:12:13 +0300

    cleanup + added ChibiOS branch

--- aea.mk
+++ aea.mk
@@ -1,7 +1,10 @@
 DEPS_DB := $(CURDIR)/dependencies.txt
 
+CHIBIOS_BRANCH := $(shell cd ${CHIBIOS} && git branch | grep \* | cut -d ' ' -f2)
+CHIBIOS_COMMIT := $(shell cd ${CHIBIOS} && git rev-parse HEAD)
+
 PRE_MAKE_ALL_RULE_HOOK:
-	@cd ${CHIBIOS} && $(eval COMMIT=$(shell git rev-parse HEAD)) echo "ChibiOS $(COMMIT)" > $(DEPS_DB)
+	@echo "ChibiOS/$(CHIBIOS_BRANCH) $(CHIBIOS_COMMIT)" > $(DEPS_DB)
 
 POST_MAKE_ALL_RULE_HOOK:
 
