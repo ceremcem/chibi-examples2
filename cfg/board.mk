08233b0ca6c64c3a6a8f24b59c8a2b384fd96e7d
Cerem Cem ASLAN <cem@aktos.io>
2019-01-27 22:10:37 +0300

    needed for successful compilation

--- cfg/board.mk
+++ cfg/board.mk
@@ -1,8 +1,9 @@
 # List of all the board related files.
-BOARDSRC = ./board.c
+#BOARDSRC = ${CHIBIOS}/os/hal/boards/ST_NUCLEO64_F103RB/board.c
+BOARDSRC = #$(CURDIR)/board.c
 
 # Required include directories
-BOARDINC = ./include
+BOARDINC = $(CURDIR)
 
 # Shared variables
 ALLCSRC += $(BOARDSRC)
