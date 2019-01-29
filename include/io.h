d081f1316d02724d7a4ab04fa9b58cfe6da30313
Cerem Cem ASLAN <cem@aktos.io>
2019-01-30 01:48:32 +0300

    switched io pins for easier PCB routing

--- include/io.h
+++ include/io.h
@@ -1,7 +1,7 @@
 // GPIOA
-#define UPWARD_BUTTON       0
-#define DOWNWARD_BUTTON     1
-#define PULSE_OUT           2
-#define DIR_OUT             3
+#define PULSE_OUT           0
+#define DIR_OUT             1
+#define UPWARD_BUTTON       2
+#define DOWNWARD_BUTTON     3
 #define LOWER_LIMIT_SWITCH  4
 #define UPPER_LIMIT_SWITCH  5
