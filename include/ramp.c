a5f22065e3ecc37518043fc06ea9162f60b8a5ec
Cerem Cem ASLAN <cem@aktos.io>
2019-02-09 00:46:26 +0300

    optimized ramp function

--- include/ramp.c
+++ include/ramp.c
@@ -7,8 +7,8 @@ extern bool motion_enable;
 THD_WORKING_AREA(wa_ramp, 128);
 THD_FUNCTION(ramp, arg) {
     (void) arg;
-    const int period = 100; // main period (optimum value: 50)
-    const int period_offset = 600;
+    const int period = 40; // main period (optimum value: 50)
+    const int period_offset = 870;
     int offset = period_offset;
     bool motion_enable0 = false;
 
@@ -18,11 +18,10 @@ THD_FUNCTION(ramp, arg) {
             offset = period_offset;
         }
         motion_enable0 = motion_enable;
-        if (offset > 0){
-            offset--;
-        }
-
         if (motion_enable){
+            if (offset > 0){
+                offset--;
+            }
             palSetPad(GPIOA, PULSE_OUT);
         	chThdSleepMicroseconds(period + offset);
             palClearPad(GPIOA, PULSE_OUT);
