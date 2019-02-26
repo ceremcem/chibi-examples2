80331eb32802d101f8517244e3d0861302e8df14
Cerem Cem ASLAN <cem@aktos.io>
2019-02-26 20:44:41 +0300

    simplified main file

--- main.h
+++ main.h
@@ -1,12 +1,2 @@
 #include "ch.h"
 #include "hal.h"
-
-/* Macro definitions */
-#define S2ST(sec) ((systime_t)((uint32_t)(sec) * (uint32_t)CH_CFG_ST_FREQUENCY))
-#define MS2ST(msec) ((systime_t)(((((uint32_t)(msec)) * ((uint32_t)CH_CFG_ST_FREQUENCY)) + 999UL) / 1000UL))
-#define MERGE_TWO_BYTES(a, b)                   ((a << 8) + b)
-#define SPLIT_TWO_BYTES(target, index, src)     target[index] = ((src & 0xff00) >> 8); target[index+1] = (src & 0xff);
-#define GET_BIT(source, bit_num) ((source >> bit_num) & 0x01)
-
-
-/* main.c */
