b3642e45cc704e7c2488b80b3a0a7259d79bc147
Cerem Cem ASLAN <cem@aktos.io>
2019-02-28 09:03:55 +0300

    default values are set

--- cfg/board.h
+++ cfg/board.h
@@ -2,85 +2,64 @@
 #define _BOARD_H_
 
 /*
- * MCU type, supported types are defined in ChibiOS/os/hal/platforms/hal_lld.h.
+ * MCU type
  */
-//#define STM32F103xB
-#define STM32F103xB // STM32F030F4P6
+#define STM32F103xB
+
+// See pal_default_config in board.c in order to see what variables to set
 
 /*
  * IO pins assignments.
  */
 /* GPIOA */
-#define PULSE_OUT               0u
-#define DIR_OUT                 1u
-#define UPWARD_BUTTON           2u
-#define DOWNWARD_BUTTON         3u
-#define LOWER_LIMIT_SWITCH      4u
-#define UPPER_LIMIT_SWITCH      5u
+#define DQ0                     0
+#define DQ1                     1
+#define DI0                     2
+#define DI1                     3
+#define DI2                     4
+#define DI3                     5
 
-/*
- * Port A setup. (see STM32F103 Reference, 9.2 GPIO registers)
- */
-//#define VAL_GPIOACRL            0x00008B44      /*  PA7...PA0 */
-//#define VAL_GPIOACRH            0x8882BBBB      /* PA15...PA8 */
-//#define VAL_GPIOAODR            0x00000000
-#define VAL_GPIOACRL            0x44444444      /*  PA7...PA0 */
-#define VAL_GPIOACRH            0x44444444      /* PA15...PA8 */
-#define VAL_GPIOAODR            0x00000000
-
+#define PULSE_OUT               DQ0
+#define DIR_OUT                 DQ1
+#define UPWARD_BUTTON           DI0
+#define DOWNWARD_BUTTON         DI1
+#define LOWER_LIMIT_SWITCH      DI2
+#define UPPER_LIMIT_SWITCH      DI3
 
 
 /*
- * Port B setup.
- * Everything output push pull
+ * Mass assignment of I/O ports.
+ * I/O ports initial setup, this configuration is established soon after reset
+ * in the initialization code.
+ * Please refer to the STM32 Reference Manual for details.
  */
-#define VAL_GPIOBCRL            0x22222222      /*  PB7...PB0 */
-#define VAL_GPIOBCRH            0x22222222      /* PB15...PB8 */
-#define VAL_GPIOBODR            0x00000000
+#define CR_DEFAULT    0x44444444
+#define ODR_DEFAULT    0x00000000
 
-/*
- * Port C setup.
- * Everything input with pull-up except:
- * PC4  - Normal input because there is an external resistor.
- * PC6  - Normal input because there is an external resistor.
- * PC7  - Normal input because there is an external resistor.
- * PC10 - Push Pull output (CAN CNTRL).
- * PC11 - Push Pull output (USB DISC).
- * PC12 - Push Pull output (LED).
- * PC13 - Push Pull output (LED).
- */
-#define VAL_GPIOCCRL            0x44848882      /*  PC7...PC0 */
-#define VAL_GPIOCCRH            0x88333388      /* PC15...PC8 */
-#define VAL_GPIOCODR            0xFFFFFFFF
+// Port A setup
+#define VAL_GPIOACRL            CR_DEFAULT      /*  PA7...PA0 */
+#define VAL_GPIOACRH            CR_DEFAULT      /* PA15...PA8 */
+#define VAL_GPIOAODR            ODR_DEFAULT
 
-/*
- * Port D setup.
- * Everything input with pull-up except:
- * PD0  - Normal input (XTAL).
- * PD1  - Normal input (XTAL).
- */
-#define VAL_GPIODCRL            0x88888833      /*  PD7...PD0 */
-// WARNING: PD0 and PD1 is remapped to be used as outputs, see board.c
-#define VAL_GPIODCRH            0x88888888      /* PD15...PD8 */
-#define VAL_GPIODODR            0xFFFFFFFF
+// Port B setup
+#define VAL_GPIOBCRL            CR_DEFAULT      /*  PA7...PA0 */
+#define VAL_GPIOBCRH            CR_DEFAULT      /* PA15...PA8 */
+#define VAL_GPIOBODR            ODR_DEFAULT
 
-/*
- * Port E setup.
- * Everything input with pull-up except:
- */
-#define VAL_GPIOECRL            0x88888888      /*  PE7...PE0 */
-#define VAL_GPIOECRH            0x88888888      /* PE15...PE8 */
-#define VAL_GPIOEODR            0xFFFFFFFF
+// Port C setup
+#define VAL_GPIOCCRL            CR_DEFAULT      /*  PA7...PA0 */
+#define VAL_GPIOCCRH            CR_DEFAULT      /* PA15...PA8 */
+#define VAL_GPIOCODR            ODR_DEFAULT
 
-/*
- * USB bus activation macro, required by the USB driver.
- */
-#define usb_lld_connect_bus(usbp) palClearPad(GPIOC, GPIOC_USB_DISC)
+// Port D setup
+#define VAL_GPIODCRL            CR_DEFAULT      /*  PA7...PA0 */
+#define VAL_GPIODCRH            CR_DEFAULT      /* PA15...PA8 */
+#define VAL_GPIODODR            ODR_DEFAULT
 
-/*
- * USB bus de-activation macro, required by the USB driver.
- */
-#define usb_lld_disconnect_bus(usbp) palSetPad(GPIOC, GPIOC_USB_DISC)
+// Port E setup
+#define VAL_GPIOECRL            CR_DEFAULT      /*  PA7...PA0 */
+#define VAL_GPIOECRH            CR_DEFAULT      /* PA15...PA8 */
+#define VAL_GPIOEODR            ODR_DEFAULT
 
 #if !defined(_FROM_ASM_)
 #ifdef __cplusplus
