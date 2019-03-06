16015a336e9bfe94da6dbe8755c56d0ad0714c1d
Cerem Cem ASLAN <cem@aktos.io>
2019-03-07 02:23:02 +0300

    refactored directory structure

--- cfg-f030x4/mcuconf.mk
+++ cfg-f030x4/mcuconf.mk
@@ -1,5 +1,5 @@
 include $(CHIBIOS)/os/hal/ports/STM32/STM32F0xx/platform.mk
+include $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/mk/startup_stm32f0xx.mk
 include $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/mk/port_v6m.mk
 MCU = cortex-m0
-include $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/mk/startup_stm32f0xx.mk
 LDSCRIPT= $(STARTUPLD)/STM32F030x4.ld
