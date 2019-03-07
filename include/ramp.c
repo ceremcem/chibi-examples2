6553c82a6f4e6e0121cc0ca58a79b02e9173f9d6
Cerem Cem ASLAN <cem@aktos.io>
2019-03-07 04:40:22 +0300

    stopping PWM on button release

--- include/ramp.c
+++ include/ramp.c
@@ -11,15 +11,15 @@ THD_FUNCTION(ramp, arg) {
     const int period_offset = pwmcfg.period;
     int offset = period_offset;
     bool motion_enable0 = false;
-
-    pwmStart(&pulse_PWM_dr, &pwmcfg);
     uint16_t percentage = 5000; // unit: 1/10_000
-    pwmEnableChannel(&pulse_PWM_dr, pulse_PWM_ch, PWM_PERCENTAGE_TO_WIDTH (&pulse_PWM_dr, percentage));
+
 
     while(!chThdShouldTerminateX()){
         if (!motion_enable0 && motion_enable){
             // rising edge, start ramp
             offset = period_offset;
+            pwmStart(&pulse_PWM_dr, &pwmcfg);
+            pwmEnableChannel(&pulse_PWM_dr, pulse_PWM_ch, PWM_PERCENTAGE_TO_WIDTH (&pulse_PWM_dr, percentage));
         }
         motion_enable0 = motion_enable;
         if (motion_enable){
@@ -29,8 +29,9 @@ THD_FUNCTION(ramp, arg) {
             pwmChangePeriod(&pulse_PWM_dr, offset); // period
             pwmEnableChannel(&pulse_PWM_dr, pulse_PWM_ch, PWM_PERCENTAGE_TO_WIDTH (&pulse_PWM_dr, percentage));
 
-            chThdSleepMilliseconds(1000);
+            chThdSleepMilliseconds(10);
         } else {
+            pwmStop(&pulse_PWM_dr);
             chThdSleepMilliseconds(1);
         }
     }
