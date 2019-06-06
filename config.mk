38ef9b430de12204bdff1fc0c12bce72e991128a
Cerem Cem ASLAN <cem@aktos.io>
2019-06-06 09:48:35 +0300

    update chibi-project

--- config.mk
+++ config.mk
@@ -2,4 +2,5 @@
 Target      := Debug
 # Hardware configuration files path 
 Hardware    := hw/f030
-GCC_Path    := $(HOME)/gcc/gcc-arm-none-eabi-5_3-2016q1/bin/
+GCC_Path    := $(HOME)/gcc/gcc-arm-none-eabi-5_3-2016q1/bin
+ChibiOS_Path:= 
